e79bc06c11b028c5b778826ce57c19a2ea95215f
Marcel Jepma <m.jepma@jispro.nl>
2017-03-29 09:45:28 +0200

    Initial status

---
+++ cmd/root.go
@@ -0,0 +1,64 @@
+// Copyright © 2017 Marcel Jepma <mjepma@xebia.com>
+// This file is part of release-support.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
+	Use:   "release-support",
+	Short: "Control your versioning",
+	Long: `Control the versioning of your GIT repositories by using this
+little tool. It gives you the ability to automatically build test and
+tag your software. If everything succeeds, it will continue.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports Persistent Flags, which, if defined here,
+	// will be global for your application.
+
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.release-support.yaml)")
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" { // enable ability to specify config file via flag
+		viper.SetConfigFile(cfgFile)
+	}
+
+	viper.SetConfigName(".release-support") // name of config file (without extension)
+	viper.AddConfigPath("$HOME")            // adding home directory as first search path
+	viper.AutomaticEnv()                    // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+// Copyright © 2017 Marcel Jepma <mjepma@xebia.com>
+// This file is part of release-support.
+
+package main
+
+import "github.com/jepma/release-support/cmd"
+
+func main() {
+	cmd.Execute()
+}
